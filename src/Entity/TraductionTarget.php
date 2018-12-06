f6e3930d319fc39ed1932eb330fd5e3e82a2941b
TurukTheCook <yoann.roule@gmail.com>
2018-12-06 10:45:40 +0100

    project schema, lang schema, trad source/target schemas

---
+++ src/Entity/TraductionTarget.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\TraductionTargetRepository")
+ */
+class TraductionTarget
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\LangCode")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $langCode;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\TraductionSource", inversedBy="traductionTargets")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $traductionSource;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLangCode(): ?LangCode
+    {
+        return $this->langCode;
+    }
+
+    public function setLangCode(?LangCode $langCode): self
+    {
+        $this->langCode = $langCode;
+
+        return $this;
+    }
+
+    public function getTraductionSource(): ?TraductionSource
+    {
+        return $this->traductionSource;
+    }
+
+    public function setTraductionSource(?TraductionSource $traductionSource): self
+    {
+        $this->traductionSource = $traductionSource;
+
+        return $this;
+    }
+}
--- src/Entity/User.php
+++ src/Entity/User.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -32,6 +34,22 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\Project", mappedBy="userId", orphanRemoval=true)
+     */
+    private $projects;
+
+    /**
+     * @ORM\OneToMany(targetEntity="App\Entity\LangHasUser", mappedBy="userId", orphanRemoval=true)
+     */
+    private $langsHasUser;
+
+    public function __construct()
+    {
+        $this->projects = new ArrayCollection();
+        $this->langsHasUser = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -104,5 +122,67 @@ class User implements UserInterface
     {
         // If you store any temporary, sensitive data on the user, clear it here
         // $this->plainPassword = null;
+    }
+
+    /**
+     * @return Collection|Project[]
+     */
+    public function getProjects(): Collection
+    {
+        return $this->projects;
+    }
+
+    public function addProject(Project $project): self
+    {
+        if (!$this->projects->contains($project)) {
+            $this->projects[] = $project;
+            $project->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProject(Project $project): self
+    {
+        if ($this->projects->contains($project)) {
+            $this->projects->removeElement($project);
+            // set the owning side to null (unless already changed)
+            if ($project->getUserId() === $this) {
+                $project->setUserId(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|LangHasUser[]
+     */
+    public function getLangsHasUser(): Collection
+    {
+        return $this->langsHasUser;
+    }
+
+    public function addLangsHasUser(LangHasUser $langsHasUser): self
+    {
+        if (!$this->langsHasUser->contains($langsHasUser)) {
+            $this->langsHasUser[] = $langsHasUser;
+            $langsHasUser->setUserId($this);
+        }
+
+        return $this;
+    }
+
+    public function removeLangsHasUser(LangHasUser $langsHasUser): self
+    {
+        if ($this->langsHasUser->contains($langsHasUser)) {
+            $this->langsHasUser->removeElement($langsHasUser);
+            // set the owning side to null (unless already changed)
+            if ($langsHasUser->getUserId() === $this) {
+                $langsHasUser->setUserId(null);
+            }
+        }
+
+        return $this;
     }
 }
