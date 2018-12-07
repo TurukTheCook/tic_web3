99f47dd4e80c5323597a2de130a4d3d348510f95
TurukTheCook <yoann.roule@gmail.com>
2018-12-07 01:24:42 +0100

    create project with name, lang and user attached

--- src/Controller/ProjectsController.php
+++ src/Controller/ProjectsController.php
@@ -4,7 +4,11 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use App\Entity\Project;
+use App\Entity\LangCode;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class ProjectsController extends AbstractController
 {
@@ -13,9 +17,35 @@ class ProjectsController extends AbstractController
      */
     public function index()
     {
-        $repo = $this->getDoctrine()->getRepository(Project::class);
-        $projects = $repo->findAll();
+        $projectsRepo = $this->getDoctrine()->getRepository(Project::class);
+        $projects = $projectsRepo->findAll();
 
         return $this->render('projects/index.html.twig', ['projects' => $projects]);
+    }
+
+    /**
+     * @Route("/projects/create", name="projects_create")
+     */
+    public function create(Request $request, UserInterface $user): Response
+    {
+        $langsRepo = $this->getDoctrine()->getRepository(LangCode::class);
+        $langs = $langsRepo->findAll();
+        
+        if ($request->isMethod('POST')) {
+            $projectName = $request->request->get('projectName');
+            $projectLangId = $request->request->get('projectLangCode');
+            $projectLangCode = $langsRepo->find($projectLangId);
+            
+            $project = new Project();
+            $project->setName($projectName);
+            $project->setLangCode($projectLangCode);
+            $project->setUserId($user);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($project);
+            $em->flush();
+            return $this->redirectToRoute('projects');
+        }
+        
+        return $this->render('projects/create.html.twig', ['langs' => $langs]);
     }
 }
